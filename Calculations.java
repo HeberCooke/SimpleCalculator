68b4cb414fab8c444c9452f94c9e650d676cc2b3
Heber Cooke <cooke.heb6520@stu.stech.edu>
2018-12-12 09:10:36 -0700

    SimpleCalculator

--- Calculations.java
+++ Calculations.java
@@ -1,38 +1,125 @@
-
+package calculator;
 
 public class Calculations {
 	static double answer = 0.0;
 	static double num1;
 	static double num2;
-	static String temp1 = "";
+	static String temp1;
 	static String temp2 = "";
 	static String sign = ""; // *,/,+,-,.,%
-	static String[] regex = { "*", "%", "/", "-", "+" }; 
+	static String[] regex = { "*", "/", "-", "+" };
 	static int i = 0;
 
+	public static double getNum1() {
+		return num1;
+	}
 
-	public static String calculation(String calc) {// method for calculating
+	public static void setNum1(double num1) {
+		Calculations.num1 = num1;
+	}
 
+	public static double getNum2() {
+		return num2;
+	}
+
+	public static void setNum2(double num2) {
+		Calculations.num2 = num2;
+	}
+
+	public static String getTemp1() {
+		return temp1;
+	}
+
+	public static void setTemp1(String temp1) {
+		Calculations.temp1 = temp1;
+	}
+
+	public static String getTemp2() {
+		return temp2;
+	}
+
+	public static void setTemp2(String temp2) {
+		Calculations.temp2 = temp2;
+	}
+
+	public static String calculationsForPercent(String calc) {
+		double num3 =0;
+		double num4 = 0;
+		int sNum1 =0;
 		for (i = 0; i < regex.length; i++) { // finding the sign
 			if (calc.contains(regex[i])) {
+				sign = regex[i];
+			}
+		}
+		i = 0;
+		sNum1 = calc.indexOf(sign);
+
+		temp1 = calc.substring(0, sNum1);
+		temp2 = calc.substring(sNum1 + 1, (calc.length() - 1));
+
+		num1 = Double.parseDouble(temp1);
+		num2 = Double.parseDouble(temp2);
+		switch (sign) {
+		case "/":  		// division-----------	done
+				num3 = num2 /100;
+				num4 = num3*num1;
+				answer = num4;
+		
+			if (num1 == 0 || num2 == 0) {
+				return " UNIVERSE BROKEN!";
+			}
+			break;
+		case "*":			// multiplication --------- done 
+		num3 = num1* num2;
+			answer =num3/100;
+			break;
+		case "-":			// subtraction 	------------- done
+			num3=0;
+			num4=0;
+			num3 = num2 / 100;
+			num4 = num1*num3;
+			answer = num1-num4;
+			break;
+		case "+":				//addition ----------------- done
+			num3=0;
+			num4=0;
+			num3 = num2 / 100;
+			num4 = num1*num3;
+			answer = num1+num4;
+			break;
+		}
+		if (temp1 != "" || temp2 != "" || sign != "") { // making sure the problem is not empty
+
+			return String.format("%.2f", answer);
+
+		} else
+			return "";
+
+	}// end calculations for percent
+
+	public static String calculation(String calc2) {// method for calculating
+		int sNum =0;
+
+		for (i = 0; i < regex.length; i++) { // finding the sign
+			if (calc2.contains(regex[i])) {
 				sign = regex[i];
 
 			}
 		}
-		int sNum = calc.indexOf(sign);
-		temp1 = calc.substring(0, sNum);
-		temp2 = calc.substring(sNum + 1);
+		 sNum = calc2.indexOf(sign);
+		temp1 = calc2.substring(0, sNum);
+		temp2 = calc2.substring(sNum + 1);
 
 		num1 = Double.parseDouble(temp1);
 		num2 = Double.parseDouble(temp2);
 
 		switch (sign) {
 		case "/":
-		
+
 			answer = num1 / num2;
 
-			if( num1 == 0 || num2 == 0) {
-				return " UNIVERSE BROKEN!";
+			if (num1 == 0 || num2 == 0) {
+				return "UNIVERSE BROKE!";
 			}
 			break;
 		case "*":
@@ -44,21 +131,13 @@ public class Calculations {
 		case "+":
 			answer = num1 + num2;
 			break;
-		case "%":
-			int  num3 = (int) Math.rint( num1 / num2);
-			System.out.println(num3);
-			int n = (int) (num2 *num3);
-			answer = num1 - n;
-			break;
+
 		}
-		if (temp1 != "" || temp2 != "" || sign != "") { //making sure the problem is not empty
-		
-			return String.format("%.2f", answer); 
-					
+		if (temp1 != "" || temp2 != "" || sign != "") { // making sure the problem is not empty
+
+			return String.format("%.2f", answer);
+
 		} else
 			return "";
 	} // end calculation
 }
-
-
-
--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,6 @@
+package calculator;
 // Heber Cooke			12/11/2018
+
 //Calculator for final OOP project
 
 import javafx.application.Application;
@@ -6,6 +8,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -22,36 +25,33 @@ public class Calculator extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		String screenStyle = new String(  //  screen styles
+		String screenStyle = new String( // screen styles
 				"-fx-background-color:lightgrey;" + "-fx-background-radius:3px;" + "-fx-border-radius:10px;"
 						+ "-fx-border-color:black;" + "-fx-border-width:3px;" + "-fx-border-style: solid outside;");
-						
+
 		String btnStyle = new String(// button styles
 				"-fx-background-color: linear-gradient(to bottom right, silver, slategrey);"
-				+ "-fx-background-radius:15px;" + "-fx-border-color:grey;" + "-fx-border-width: 2px;"
-				+ "-fx-border-radius:15px;" + "-fx-font-family: Times New Roman, Times, serif;"
-				+ "-fx-font-weight: bold;" + " -fx-font-size: 30px;"+ "-fx-font-color:black");
+						+ "-fx-background-radius:15px;" + "-fx-border-color:grey;" + "-fx-border-width: 2px;"
+						+ "-fx-border-radius:15px;" + "-fx-font-family: Times New Roman, Times, serif;"
+						+ "-fx-font-weight: bold;" + " -fx-font-size: 30px;" + "-fx-font-color:black");
 
 		BorderPane pane = new BorderPane(); // main pane
 		VBox boxForView = new VBox(); // vBox for views
-		
+
 		TextField tf1 = new TextField(); // text field for main display
 		tf1.setFont(new Font("Times", 35));
 		tf1.setAlignment(Pos.TOP_RIGHT);
 		tf1.setEditable(false);
 		tf1.setStyle(screenStyle);
-		
+
 		TextField tf2 = new TextField();// text field for history
+		tf2.setMaxHeight(15);
 		tf2.setStyle(screenStyle);
-		tf2.setAlignment(Pos.BASELINE_RIGHT);
-		tf2.setFont(new Font("Times", 12));
+		tf2.setFont(new Font("Times", 14));
 		tf2.setDisable(true);
-		
-		
+		tf2.setAlignment(Pos.BASELINE_LEFT);
 		GridPane btnPane = new GridPane(); // pane for buttons
 		btnPane.setStyle("-fx-background-color:grey;" + "-fx-padding: 15;" + "-fx-hgap: 15;" + "-fx-vgap: 10;");
-
-	
 
 		Button btn0 = new Button("<--");
 		Button btn01 = new Button("C");
@@ -74,11 +74,12 @@ public class Calculator extends Application {
 		Button btnEquals = new Button("=");
 		GridPane.setColumnSpan(btnEquals, 2); // double size for =
 
-		btn0.setStyle("-fx-background-color: linear-gradient(to bottom right, silver, slategrey);"// special font for the <-- button
+		btn0.setStyle("-fx-background-color: linear-gradient(to bottom right, silver, slategrey);"// special font for
+																									// the <-- button
 				+ "-fx-background-radius:15px;" + "-fx-border-color:grey;" + "-fx-border-width: 2px;"
 				+ "-fx-border-radius:15px;" + "-fx-font-family: Times New Roman, Times, serif;"
-				+ "-fx-font-weight: bold;" + " -fx-font-size: 25px;"); 
-				
+				+ "-fx-font-weight: bold;" + " -fx-font-size: 25px;");
+
 		btn1.setStyle(btnStyle);
 		btn2.setStyle(btnStyle);
 		btn3.setStyle(btnStyle);
@@ -95,7 +96,7 @@ public class Calculator extends Application {
 		btnDivide.setStyle(btnStyle);
 		btnSubtract.setStyle(btnStyle);
 		btnAdd.setStyle(btnStyle);
-		btnDec.setStyle(btnStyle);	
+		btnDec.setStyle(btnStyle);
 		btnEquals.setStyle(btnStyle);
 		// -------------------------------
 		btnEquals.setPrefSize(180, 80);
@@ -142,42 +143,63 @@ public class Calculator extends Application {
 		// ---------------------------------------
 
 		btn1.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText("");
+			}
 			tf1.setText(tf1.getText() + "1");
-
 		});
 		btn2.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText("");
+			}
 			tf1.setText(tf1.getText() + "2");
-
 		});
 		btn3.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText("");
+			}
 			tf1.setText(tf1.getText() + "3");
-
 		});
 		btn4.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText("");
+			}
 			tf1.setText(tf1.getText() + "4");
-
 		});
 		btn5.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText("");
+			}
 			tf1.setText(tf1.getText() + "5");
-
 		});
 		btn6.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText("");
+			}
 			tf1.setText(tf1.getText() + "6");
-
 		});
 		btn7.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText("");
+			}
 			tf1.setText(tf1.getText() + "7");
-
 		});
 		btn8.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText("");
+			}
 			tf1.setText(tf1.getText() + "8");
-
 		});
 		btn9.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText("");
+			}
 			tf1.setText(tf1.getText() + "9");
-
 		});
 		btn00.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText("");
+			}
 			tf1.setText(tf1.getText() + "0");
 
 		});
@@ -186,10 +208,16 @@ public class Calculator extends Application {
 
 		});
 		btn02.setOnAction(e -> {
-			tf1.setText(tf1.getText() + "%");
+			if (tf1.getText().contains("+") || tf1.getText().contains("-") || tf1.getText().contains("/")
+					|| tf1.getText().contains("*")) {
+				tf1.setText(tf1.getText() + "%");
+			}
 
 		});
 		btn03.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText(tf1.getText().substring(1));
+			}
 			if (!tf1.getText().contains("*")) {
 				tf1.setText(tf1.getText() + "*");
 			}
@@ -200,16 +228,25 @@ public class Calculator extends Application {
 			}
 		});
 		btnDivide.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText(tf1.getText().substring(1));
+			}
 			if (!tf1.getText().contains("/")) {
 				tf1.setText(tf1.getText() + "/");
 			}
 		});
 		btnAdd.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText(tf1.getText().substring(1));
+			}
 			if (!tf1.getText().contains("+")) {
 				tf1.setText(tf1.getText() + "+");
 			}
 		});
 		btnSubtract.setOnAction(e -> {
+			if (tf1.getText().contains("=")) {
+				tf1.setText(tf1.getText().substring(1));
+			}
 			if (!tf1.getText().contains("-")) {
 				tf1.setText(tf1.getText() + "-");
 			}
@@ -220,7 +257,7 @@ public class Calculator extends Application {
 		});
 		btnDec.setOnAction(e -> {
 
-			if (!tf1.getText().contains(".")) { // Decimal set to do one decmal per number
+			if (!tf1.getText().contains(".")) { // Decimal set to do one decimal per number
 
 				tf1.setText(tf1.getText() + ".");
 			}
@@ -231,23 +268,40 @@ public class Calculator extends Application {
 			}
 
 		});
-		btnEquals.setOnAction(e -> {    // EQUALS Button And Call calculate method
-			if(tf1.getText().contains("/")|| tf1.getText().contains("*") || tf1.getText().contains(("-")) || tf1.getText().contains("+")|| tf1.getText().contains("%")) {
-				tf2.setText(tf2.getText() + tf1.getText()+ "="+ Calculations.calculation(tf1.getText())+ "      "); // setting the history screen
-				tf1.setText(Calculations.calculation(tf1.getText())); // calculations
-			
+		btnEquals.setOnAction(e -> { // EQUALS Button And Call calculate method
+			if (tf1.getText().contains("%")) { // percent calculations
+				if (tf1.getText().contains("/") || tf1.getText().contains("*") || tf1.getText().contains(("-"))
+						|| tf1.getText().contains("+")) {
+					tf2.setText(tf2.getText() + tf1.getText() + "=" + Calculations.calculationsForPercent(tf1.getText())
+							+ "      "); // setting the history screen
+
+					tf1.setText("=" + Calculations.calculationsForPercent(tf1.getText())); // calculations and setting
+																							// to the main screen
+
+				}
+			}
+			// // EQUALS Button And Call calculate method NON percent calculations
+			if (tf1.getText().contains("/") || tf1.getText().contains("*") || tf1.getText().contains(("-"))
+					|| tf1.getText().contains("+")) {
+				// setting the history screen
+				if(tf2.getText().length() > 40) {	// setting the history screen to remove old calculations
+					tf2.setText(tf2.getText().substring(40));
+				}
+				tf2.setText(tf2.getText() + tf1.getText() + "=" + Calculations.calculation(tf1.getText()) + "      ");
+
+				tf1.setText("=" + Calculations.calculation(tf1.getText())); // calculations and setting to the main
+																				// screen
+
 			}
 
-			
 		});
 
-		
 		boxForView.setStyle("-fx-background-color:grey");
 		boxForView.getChildren().addAll(tf2, tf1); // add text view to VBox
-		
-		btnPane.getChildren().addAll(btn0, btn01, btn02, btn03,// adding all buttons
-				 btn7, btn8, btn9, btnDivide, btn4, btn5, btn6,
-				btnSubtract, btn1, btn2, btn3, btnAdd, btn00, btnDec, btnEquals); 
+
+		btnPane.getChildren().addAll(btn0, btn01, btn02, btn03, // adding all buttons
+				btn7, btn8, btn9, btnDivide, btn4, btn5, btn6, btnSubtract, btn1, btn2, btn3, btnAdd, btn00, btnDec,
+				btnEquals);
 
 		pane.setTop(boxForView); // view screen to pane
 		pane.setCenter(btnPane);// set buttons to pane
@@ -257,7 +311,7 @@ public class Calculator extends Application {
 		VBox.setMargin(tf2, new Insets(10, 20, 10, 20));
 
 		Scene scene = new Scene(pane, 400, 600);
-	
+
 		primaryStage.setResizable(false); // resize false
 		primaryStage.setTitle("Calculator");
 		primaryStage.setScene(scene);
@@ -266,6 +320,3 @@ public class Calculator extends Application {
 	}
 
 }// end main ---------------------------------------------
-
-
-
